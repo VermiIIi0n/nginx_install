05cfbfd2fc1b641bf66ffccfb8f9a0afbc0294af
VermiIIi0n <104612478+VermiIIi0n@users.noreply.github.com>
2024-03-07 18:22:58 +0800

    tweak benchmark output

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -3,7 +3,7 @@ if [ ! -d "wrk" ]; then
     sudo apt-get install -y git # Install git package
     git clone https://github.com/wg/wrk.git
     cd wrk || exit 1
-    make -j `nproc` || exit 1
+    make -j `nproc` > /dev/null 2>&1 || exit 1
     cd ..
 fi
 
